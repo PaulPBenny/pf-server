24914be0a746908b2de5e6b4fccb3c31867b6750
PaulPBenny <127484453+PaulPBenny@users.noreply.github.com>
2024-07-15 13:06:50 +0530

    first commit

---
+++ Controller/projectController.js
@@ -0,0 +1,129 @@
+
+const projects =require('../Models/projectSchema')
+
+
+ exports.addProjects =async (req,res)=>{
+    console.log("inside add project function")
+    const userId=req.payload
+    const projectImage=req.file.filename
+    const {title,languages,github,website,overview}=req.body
+    //console.log(` ${title} ,${languages} ,${github} ,${website} ,${overview} ,${projectImage},${userId}`)
+   
+
+    try {
+
+        const existingProject=await projects.findOne({github})
+        if(existingProject){
+            res.status(406).json('project already exists ...upload another one')
+        }
+        else{
+            const newProject=new projects({
+    
+        title,languages,github,website,overview,projectImage,userId  
+            })
+            await newProject.save()
+            res.status(200).json(newProject)
+        }
+        
+    } catch (error) {
+        res.status(401).json(`Request failed $(error)`)
+    }
+}
+
+
+//get projects
+
+exports.allUserprojects=async(req,res)=>
+{
+    const userId=req.payload
+    try{
+        const userProjects=await projects.find({userId})
+        res.status(200).json(userProjects)
+
+    }
+    catch(error)
+    {
+        res.status(401).json(error)
+
+    }
+}
+
+//getall projects
+
+exports.allProjects=async(req,res)=>{
+
+    const searchKey=req.query.search
+    const query={
+        languages:{$regex:searchKey ,$options:"i"}
+    }
+
+    try {
+
+        const allProjects=await projects.find(query)
+        res.status(200).json(allProjects)
+        
+    } catch (error) {
+        res.status(401).json(error)
+    }
+}
+
+//home projects
+
+exports.getHomeProjects=async(req,res)=>{
+
+    try {
+
+        const homeProjects=await projects.find().limit(3)
+        res.status(200).json(homeProjects)
+        
+    } catch (error) {
+        res.status(401).json(error)
+    }
+}
+
+
+//edit project
+
+// exports.editProjectController= async(req,res)=>{
+//     //get-project id
+//     const {id}=req.params
+//     const userId=req.payload
+//     const {title,languages,github,website,overview,projectImage}=req.body
+//     const uploadProjectImage=req.file?req.file.filename:projectImage
+
+//     try {
+
+//         const updateProject = await projects.findByIdAndUpdate({_id:id},{
+//             title,languages,github,website,overview,projectImage:uploadProjectImage,userId
+//         },{new:true})
+
+//         await updateProject.save()
+//         res.status(200).json(updateProject)
+        
+//     } catch (error) {
+//         res.status(401).json(error)
+        
+//     }
+// }
+
+//edit project
+
+exports.editProjectController = async (req,res)=>{
+    //get project id
+    const {id} = req.params
+    const userId = req.payload
+    const {title,languages,github,website,overview,projectImage} = req.body
+    const uploadProjectImage = req.file?req.file.filename:projectImage
+
+    try{
+        const updateProject = await projects.findByIdAndUpdate({_id:id},{
+            title,languages,github,website,overview,projectImage:uploadProjectImage,userId
+        },{new:true})
+        await updateProject.save()
+        res.status(200).json(updateProject)
+
+    }catch (err){
+        res.status(401).json(err)
+    }
+    
+}
---
+++ Router/router.js
@@ -0,0 +1,46 @@
+const express=require('express');
+ const router =new express.Router();
+ const userController=require('../Controller/userController');
+ const projectController=require('../Controller/projectController')
+const jwtMiddleware = require('../Middlewares/jwtMiddleware');
+const multerConfig = require('../Middlewares/multerMiddleware');
+
+
+ router.post('/user/register',userController.register)
+
+
+ router.post("/user/login",userController.login);
+
+ //add project
+
+router.post("/projects/add",jwtMiddleware,multerConfig.single('projectImage'),projectController.addProjects)
+
+//get user project
+
+router.get('/user/allprojects',jwtMiddleware,projectController.allUserprojects)
+
+//get all projects
+
+router.get('/projects/all',jwtMiddleware,projectController.allProjects)
+
+
+//home projects
+
+router.get('/projects/homeprojects',projectController.getHomeProjects)
+
+//edit project
+
+router.put('/projects/edit/:id',jwtMiddleware,multerConfig.single('projectImage'),projectController.editProjectController)
+
+
+
+
+
+
+
+
+
+ 
+
+//export router
+ module.exports=router
